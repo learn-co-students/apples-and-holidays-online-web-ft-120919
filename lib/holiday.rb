a4fe464a7663d6ae13da687e5694881f1eb054ee
Steven Kumarsingh <stevenks17@gmail.com>
2019-12-13 19:03:46 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holiday|
+  puts "#{season.capitalize}:"
+     holiday.each do |holiday, item|
+       puts " #{holiday} + 
 end
 
 def all_holidays_with_bbq(holiday_hash)
